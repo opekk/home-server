3c7239a8c7cec484b459dffeb5d5734f9195f70a
opekk <maciek09874@gmail.com>
2026-04-03 17:42:58 +0200

    Revert "Fix webhook git fetch permission error on bind-mounted repos"
    This reverts commit 26e4dedd0a1c2e2b7ef09ce3659aa661194ce8b3.

--- deploy/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Fix ownership of cloned repo directories so webhook user can write
-# (bind-mounted paths may be owned by the host user with a different UID)
-if [ -d /app/repo/projects ]; then
-    chown -R webhook:webhook /app/repo/projects 2>/dev/null || true
-fi
-
-exec su-exec webhook python webhook-listener.py
